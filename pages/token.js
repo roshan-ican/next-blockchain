20d927c92f1280def16c5dbc0aedcdf8d533796e
roshan-ican <roshansahani535@gmail.com>
2022-10-10 19:07:51 +0530

    attached with the navbar

---
+++ pages/token.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+
+const token = () => {
+  return (
+    <>
+      <Navbar />
+
+      <Footer />
+    </>
+  )
+}
+
+export default token
